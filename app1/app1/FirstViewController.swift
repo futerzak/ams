adfebd0f9922748d5dd13fa5a5b7f8eaebc02d38
Użytkownik Gość <Guest@imac-16.local>
2016-12-02 10:25:34 +0100

    init commit

---
+++ app1/app1/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  app1
+//
+//  Created by Użytkownik Gość on 02.12.2016.
+//  Copyright © 2016 Użytkownik Gość. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ app1/app1/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  app1
+//
+//  Created by Użytkownik Gość on 02.12.2016.
+//  Copyright © 2016 Użytkownik Gość. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
